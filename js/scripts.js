1ac3f81f16693d21a8d81d01a785cdc10e2c5ba8
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-06-20 18:05:05 +0000

    updated hamburger menu functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,35 @@
-const buttonMenu = document.querySelector("#header__button");
-const buttonImage = document.querySelector("#header__button--image");
+const buttonMenu = document.querySelector("#header-button");
+const buttonImage = document.querySelector("#header-button-image");
 
-const navigationMenu = document.querySelector("#header__nav");
-const navigationLinks = document.querySelectorAll(".header__list--item");
+const navigationMenu = document.querySelector("#header-nav");
+const navigationLinks = document.querySelectorAll(".header-list-item");
 
-const navigationIcons = ["../images/icons/burguer.svg", "../images/icons/x-icon.svg"];
+const navigationIcons = ["./assets/burguer.svg", "./assets/x-icon.svg"];
+navigationLinks.forEach((element) => {
+	element.addEventListener("click", () => {
+		toggleNavigation();
+		changeButtonIcon(navigationIcons[0]);
+		buttonMenu.style.width = "initial";
+	});
+});
+
+buttonMenu.addEventListener("click", () => {
+	const actualButtonIcon = buttonImage.src;
+
+	if (actualButtonIcon.includes("burguer")) {
+		changeButtonIcon(navigationIcons[1]);
+		buttonMenu.style.width = "12px";
+	} else {
+		changeButtonIcon(navigationIcons[0]);
+		buttonMenu.style.width = "initial";
+	}
+	toggleNavigation();
+});
 
 function changeButtonIcon(path) {
 	buttonImage.src = path;
 }
 
 function toggleNavigation() {
-	navigationMenu.classList.toggle("header__nav--active");
+	navigationMenu.classList.toggle("header-nav-active");
 }
