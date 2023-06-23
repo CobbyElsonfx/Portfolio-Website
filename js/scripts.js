cd660849660cd4d6b5bbf377c7fef27ac5038afb
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-06-23 01:21:43 +0000

    added popup window

--- js/scripts.js
+++ js/scripts.js
@@ -34,60 +34,66 @@ buttonMenu.addEventListener('click', () => {
 });
 
 // pop up window
+/* Portfolio Section */
 
+// PortfolioArray: Contains all the Information for each Portfolio Card.
 const portfolioArray = [
   {
+    id: '0',
     name: 'Tonic',
     profile: 'Canopy',
     categories: ['Back End Dev', '2015'],
     description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required',
+        'A daily selection of privately personalized reads; no accounts or sign-ups required',
     image: './assets/Snapshoot Portfolio.png',
     technologies: ['HTML', 'CSS', 'JavaScript'],
     live: '#',
     source: '#',
+    class: '',
   },
   {
+    id: '1',
     name: 'Multi-Post Stories',
     profile: 'Facebook',
     categories: ['Full Stack Dev', '2015'],
     description:
-      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+        'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
     image: './assets/colorful.png',
     technologies: ['HTML', 'Ruby on Rails', 'CSS', 'JavaScript'],
     live: '#',
     source: '#',
+    class: 'grid-reversed',
   },
   {
+    id: '2',
     name: 'Facebook 360',
     profile: 'Facebook',
     categories: ['Full Stack Dev', '2015'],
     description:
-      'Exploring the future of media in Facebook`s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+        'Exploring the future of media in Facebook`s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
     image: './assets/Snapshoot Portfolio.png',
     technologies: ['HTML', 'Ruby on Rails', 'CSS', 'JavaScript'],
     live: '#',
     source: '#',
+    class: '',
   },
   {
+    id: '3',
     name: 'Uber Navigation',
     profile: 'Uber',
     categories: ['Lead Developer', '2018'],
     description:
-      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+        'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
     image: './assets/Multi-post.png',
     technologies: ['HTML', 'Ruby on Rails', 'CSS', 'JavaScript'],
     live: '#',
     source: '#',
+    class: 'grid-reversed',
   },
 ];
 
 const bodyElement = document.querySelector('#body');
-const portfolioButtons = document.querySelectorAll('.card-button');
-
-function showWorkContent(element) {
-  bodyElement.appendChild(element);
-}
+const portfolioElement = document.querySelector('#portfolio');
 
 function createWorkWindow(index) {
   const portfolioData = portfolioArray[index];
@@ -103,67 +109,141 @@ function createWorkWindow(index) {
     source,
   } = portfolioData;
 
-  const modalItem = document.createElement(`div`);
+  const modalItem = document.createElement('div');
+  modalItem.id = 'modal';
   modalItem.classList.add('modal');
   modalItem.innerHTML = `
-  <article class="modal-card">
-    <header class="modal-header ">
-      <h2 class="modal-title">${name}</h2>
-      <button class="modal-button">
-        <img
-          id="modal-button-image"
-          class="header-toggle"
-          src="./assets/x-icon.svg"
-          alt="close menu icon"
-        />
-      </button>
-    </header>
-    <div class="modal-content flex-column">
-      <div class="modal-profile flex-row align-center">
-        <h3 class="uppercase color-info">${profile}</h3>
-        <ul class="modal-list flex-row">
-          ${categories
-            .map((item) => `<li class="modal-item">${item}</li>`)
-            .join('')}
-        </ul>
-      </div>
-      <img
-        src="${image}"
-        alt="Work 1"
-        class="modal-image"
-      />
-      <p class="modal-description">
-        ${description}
-      </p>
-      <ul class="modal-tags">
-         ${technologies
-           .map((item) => `<li class="modal-tag">${item}</li>`)
-           .join('')}
-      </ul>
-      <hr/>
-      <div class="modal-actions">
-        <a
-          id="0"
-          href="${live}"
-          class="button button-primary modal-button"
-          >See live</a
-        >
-        <a
-          id="0"
-          href="${source}"
-          class="button modal-button"
-          >See source</a
-        >
-      </div>
-    </div>
-  </article>
-`;
-  showWorkContent(modalItem);
+            <article class="modal-card">
+              <header class="modal-header">
+                <h2 class="modal-title">${name}</h2>
+                <button id="close-modal-button"  class="close-button">
+                  <img
+                    id="close-modal-button"
+                    src="./assets/modalClosebutton.svg"
+                    alt="close button"
+                    class="close-button"
+                  />
+                </button>
+              </header>
+              <div class="modal-content">
+                <div class="modal-profile">
+                  <h3 class=" color-info">${profile}</h3>
+                  <ul class="modal-list ">
+                    ${categories
+    .map((item) => `<img class="counter" src="./assets/Counter.png"></img><li class="modal-item">${item}</li>`)
+    .join('')}
+                  </ul>
+                </div>
+                <img
+                  src="${image}"
+                  alt="Work 1"
+                  class="modal-image"
+                />
+               <div class="modal-details grid grid-uneven-two">
+                <p class="modal-description">
+                  ${description}
+                </p>
+                <div class="modal-details inner ">
+                  <ul class="modal-tags ">
+                    ${technologies
+    .map((item) => `<li class="modal-tag">${item}</li>`)
+    .join('')}
+                  </ul>
+                  <hr class="modal-divider" />
+                  <div class="modal-actions flex-row">
+                    <a
+                      id="0"
+                      href="${live}"
+                      target="_blank"
+                      class="button button-primary modal-button"
+                      >See live 
+                      <img
+                        src="./assets/modalliveicons.svg"
+                        alt="Live icon dark version"
+                      /></a
+                    >
+                    <a
+                      id="0"
+                      href="${source}"
+                      target="_blank"
+                      class="button button-primary modal-button"
+                      >See source
+                        <img
+                        src="./assets/modalgithub.svg"
+                        alt="Github icon dark version"
+                      />
+                      </a
+                    >
+                  </div>
+                </div>
+               </div>
+              </div>
+            </article>
+         `;
+
+  bodyElement.style.overflow = 'hidden';
+  bodyElement.appendChild(modalItem);
+
+  const modalCloseButton = document.querySelector('#close-modal-button');
+  modalCloseButton.addEventListener('click', () => {
+    bodyElement.removeChild(modalItem);
+    bodyElement.style.overflow = 'auto';
+  });
 }
 
-portfolioButtons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    const index = event.target.id;
-    createWorkWindow(index);
+// AddPortfolioWork(): Put every Portfolio Card to his container.
+function addPortfolioWork() {
+  portfolioElement.innerHTML = `
+  ${portfolioArray
+    .map(
+      (item) => `<article class="portfolio-card" >
+              <header class="portfolio-header ${item.class}">
+                <img
+                  src="${item.image}"
+                  alt="Work 1"
+                  class="portfolio-image"
+                />
+              </header>
+              <div class="portfolio-content">
+                <h2 class="portfolio-title ">${item.name}</h2>
+                <div class="portfolio-profile">
+                  <h3>${item.profile}</h3>
+                  <ul class="portfolio-list">
+                      ${item.categories
+    .map(
+      (item) => `<img class="counter" src="./assets/Counter.png"></img><li class="portfolio-item">${item}</li>`,
+    )
+    .join('')}
+                  </ul>
+                </div>
+                <p class="primary-description">
+                  ${item.description}
+                </p>
+                <ul class="portfolio-tags ">
+                  ${item.technologies
+    .map((item) => `<li class="portfolio-tag">${item}</li>`)
+    .join('')}
+                </ul>
+                <div class="portfolio-actions">
+                  <button
+                    id="${item.id}"
+                    href="#"
+                    class="button button-primary portfolio-button"
+                    >See project</button>
+                </div>
+              </div>
+            </article>`,
+    )
+    .join('')}`;
+
+  const portfolioButtons = document.querySelectorAll('.portfolio-button');
+  portfolioButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const index = event.target.id;
+      createWorkWindow(index);
+    });
   });
-});
+}
+
+// Called  the Function to fill the PortfolioElement.
+addPortfolioWork();
